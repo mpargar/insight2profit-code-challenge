30f8888eb6f94e13f851f38169964a5eeaeca116
Miguel Parra <miguel.parra@distillery.com>
2022-03-29 14:31:51 -0700

    test(FloatingLogos):

--- src/components/FloatingLogos/FloatingLogos.js
+++ src/components/FloatingLogos/FloatingLogos.js
@@ -2,13 +2,14 @@ import React from "react";
 import distilleryLogo from "../../img/distillery.svg";
 import insight2profitLogo from "../../img/insight2profit.svg";
 import styles from "./FloatingLogos.module.scss";
+import {DISTILLERY_LOGO_ALT, INSIGHT2PROFIT_LOGO_ALT} from "./FloatingLogosContants";
 const FloatingLogos = () => (
   <div className={styles.floatingLogosContainer}>
-    <img src={distilleryLogo} className="logo" alt="Distillery logo" />
+    <img src={distilleryLogo} className="logo" alt={DISTILLERY_LOGO_ALT} />
     <img
       src={insight2profitLogo}
       className="logo"
-      alt="Insight 2 profit logo"
+      alt={INSIGHT2PROFIT_LOGO_ALT}
     />
   </div>
 );
---
+++ src/components/FloatingLogos/FloatingLogos.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import FloatingLogos from "./FloatingLogos";
+import {
+  DISTILLERY_LOGO_ALT,
+  INSIGHT2PROFIT_LOGO_ALT,
+} from "./FloatingLogosContants";
+import distilleryLogo from "../../img/distillery.svg";
+import insight2profitLogo from "../../img/insight2profit.svg";
+
+describe("Floating Logos", () => {
+  it("Should display container and their childs correctly", () => {
+    const { debug, container } = render(<FloatingLogos />);
+    debug();
+    const wrapper = container.getElementsByClassName("floatingLogosContainer");
+    const images = wrapper?.[0]?.querySelectorAll("img");
+    expect(wrapper.length).toBe(1);
+    expect(images.length).toBe(2);
+  });
+
+  it("Should render distillery logo", () => {
+    const { getByAltText } = render(<FloatingLogos />);
+    const image = getByAltText(DISTILLERY_LOGO_ALT);
+    expect(image.src).toContain(distilleryLogo);
+    expect(image.className).toContain("logo");
+  });
+
+  it("Should render insight2profit logo", () => {
+    const { getByAltText } = render(<FloatingLogos />);
+    const image = getByAltText(INSIGHT2PROFIT_LOGO_ALT);
+    expect(image.src).toContain(insight2profitLogo);
+    expect(image.className).toContain("logo");
+  });
+});
